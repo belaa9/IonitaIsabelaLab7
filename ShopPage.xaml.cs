ab585433ca150a9bd2d41831fa7ea46aee8e8192
belaa9 <isabela.ionita09@gmail.com>
2024-12-10 01:44:04 +0200

    Laborator 10 continuare

--- ShopPage.xaml.cs
+++ ShopPage.xaml.cs
@@ -1,4 +1,5 @@
 using IonitaIsabelaLab7.Models;
+using Plugin.LocalNotification;
 
 namespace IonitaIsabelaLab7;
 
@@ -24,9 +25,24 @@ public partial class ShopPage : ContentPage
         {
             Name = "Magazinul meu preferat" };
         var shoplocation = locations?.FirstOrDefault();
-            /* var shoplocation= new Location(46.7492379, 23.5745597);//pentru
-            Windows Machine */
+        var myLocation = await Geolocation.GetLocationAsync();
+        /* var myLocation = new Location(46.7731796289, 23.6213886738);
+       //pentru Windows Machine */
+        var distance = myLocation.CalculateDistance(shoplocation,DistanceUnits.Kilometers);
+        if (distance < 5)
+        {
+            var request = new NotificationRequest
+            {
+                Title = "Ai de facut cumparaturi in apropiere!",
+                Description = address,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(1)
+                }
+            };
+            LocalNotificationCenter.Current.Show(request);
+        }
 
-            await Map.OpenAsync(shoplocation, options);
+        await Map.OpenAsync(shoplocation, options);
         }
 }
